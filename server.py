c43cb23470e701054b4f61d7d4e3d30c1fb6dca5
Katja Schwarz <katja.schwarz@stud.uni-heidelberg.de>
2019-01-16 13:40:15 +0100

    Merge remote-tracking branch 'remotes/upstream/master'
    added python code formally nice
    
    # Conflicts:
    #	package-lock.json
    #	src/util/resizePics.js

--- server.py
+++ server.py
@@ -14,24 +14,36 @@ import json
 import time, threading
 import requests
 from random import uniform
-from MapNetCode.initialization import initialize
-from MapNetCode.communication import make_nodes, read_nodes
-from MapNetCode.train import train, get_modified, get_neighborhood, \
-    mutual_k_nearest_neighbors, svm_k_nearest_neighbors, \
-    listed_k_nearest_neighbors, score_k_nearest_neighbors, \
-    reset, select_neighbors
-
+import python_code.initialization as init
+import python_code.communication as communication
+from python_code.aux import scale_to_range
 import pickle
 
+
 # initialize global dataset information (image ids, features, embedding) and network
-dataset_info = None
+dataset_name = 'Wikiart_Elgammal_test'
+feature_dim = 512
+limits = (-15, 15)
+max_display = 100       # show at most max_display images in interface
+data_info_file = './MapNetCode/pretraining/wikiart_datasets/info_elgammal_subset_test.hdf5'
+print('Initialize...')
+initial_data, initial_data_info = init.get_data(dataset_name=dataset_name, impath=None,
+                                                normalization_file=None, info_file=data_info_file,
+                                                feature_dim=feature_dim)
+print('Done.')
+dataframe = None
 net = None
-experiment_id = 'TEST'#time.strftime('%m-%d-%H-%M') + '_dropout'
-N = None
-dataset = 'wikiart_elgammal'            # ['shape', 'wikiart', 'office', 'bam']
-_neighbors = {'positive': [], 'negative': []}
+experiment_id = 'TEST'#'{}_{}'.format(time.strftime('%m-%d-%H-%M'), dataset_name)
 
 StartTime = time.time()
+
+
+def reset():
+    """Reset the global variables."""
+    global dataframe, net, experiment_id
+    dataframe = None
+    net = None
+    experiment_id = 'TEST'  # '{}_{}'.format(time.strftime('%m-%d-%H-%M'), dataset_name)
 
 
 def update_embedding_handler(socket_id):
@@ -120,7 +132,7 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
         Liest den Body aus - gibt in zum konvertieren weiter
 
         """
-        global dataset_info, net, experiment_id, N, dataset, _neighbors
+        global dataframe
         if self.path == "/nodes":
             print("post /nodes")
             ### POST Request Header ###
@@ -139,31 +151,16 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
             # print(data)
 
             # Katjas code goes here
-            reset(experiment_id, dataset=dataset)
-            _neighbors = {'positive': [], 'negative': []}
-            net, dataset_info = initialize(dataset=dataset, experiment_id=experiment_id, batch_size=100,
-                                           lr=1e-4)
-            experiment_id = dataset_info['experiment_id']
-            # introduce scale factor
-            limits = (-15, 15)
-            pmax, pmin = dataset_info['position'].max(), dataset_info['position'].min()
-            dataset_info['scale_func'] = lambda x: np.divide((limits[1]-limits[0]) * (x.copy() - pmin), pmax-pmin) + limits[0]
-            dataset_info['inverse_scale_func'] = lambda x: np.divide((x.copy()-limits[0]) * (pmax-pmin), limits[1]-limits[0]) + pmin
-            if N is None:
-                N = len(dataset_info['name'])
+            reset()
+            graph_df = communication.make_graph_df(image_ids=initial_data['image_id'],
+                                                   projection=initial_data['projection'],
+                                                   info_df=initial_data_info,
+                                                   coordinate_range=limits)
 
-            nodes = make_nodes(position=dataset_info['scale_func'](dataset_info['position'][:N]),
-                               name=dataset_info['name'][:N],
-                               label=dataset_info['label'][:N],
-                               index=True)
-            categories = dataset_info['categories']
-            data = {'nodes': nodes, 'categories': categories}
+            graph_json = communication.graph_df_to_json(graph_df, max_elements=max_display)
+            self.wfile.write(graph_json)  #body zurueckschicken
+            # print(graph_json)
 
-            print(nodes)
-
-            # make json
-            data = json.dumps(data).encode()
-            self.wfile.write(data)  #body zurueckschicken
 
         if self.path == "/trainSvm":
             print("post /trainsvm")
@@ -250,24 +247,12 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
             # data = json.loads(str(body, encoding='utf-8'))      # python 3
 
             print(data)
-            print(len(data['positives']))
 
-            try:
-                _neighbors['negative'].extend(data['negatives'])
-            except KeyError:
-                pass
 
-            _neighbors['positive'], scores = select_neighbors(dataset_info['feature'][:N],
-                                                              data['positives'], _neighbors['negative'],
-                                                              k=2000, neighbor_fn=svm_k_nearest_neighbors, test=False)
 
-            print('New neighbors {}'.format(_neighbors['positive']))
-            # Katjas code goes here
-            # katja_function(data.p, data.n)
-
-            # make json
-            data['neighbours'] = {neigh: score for neigh, score in zip(_neighbors['positive'], scores)}
-            print(len(data['positives']))
+            # # make json
+            # data['neighbours'] = {neigh: score for neigh, score in zip(_neighbors['positive'], scores)}
+            # print(len(data['positives']))
             data['group'] = list(data['positives'])
             data = json.dumps(data).encode()
             self.wfile.write(data)  #body zurueckschicken
@@ -300,14 +285,14 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
             new_position = dataset_info['inverse_scale_func'](new_position)
             old_position = dataset_info['position']
 
-            idx_modified = get_modified(old_position[:N], new_position, tol=dataset_info['inverse_scale_func'](np.array([3,])))
+            idx_modified = get_modified(old_position[:max_display], new_position, tol=dataset_info['inverse_scale_func'](np.array([3, ])))
             if len(idx_modified) == 0 or len(idx_modified) == len(dataset_info['name']):       # TODO: fix recall bug
                 print('Modified {} samples. - Invalid for training.')
                 return 0
             # idx_old_neighbors = get_neighborhood(old_position[:N], idx_modified)
             # idx_new_neighbors = get_neighborhood(new_position[:N], idx_modified)
-            idx_old_neighbors, _ = mutual_k_nearest_neighbors(old_position[:N], idx_modified, k=50)
-            idx_new_neighbors = get_neighborhood(new_position[:N], idx_modified)
+            idx_old_neighbors, _ = mutual_k_nearest_neighbors(old_position[:max_display], idx_modified, k=50)
+            idx_new_neighbors = get_neighborhood(new_position[:max_display], idx_modified)
 
             print('Train MapNet using {} positives and {} negatives.'.format(len(idx_modified) + len(idx_new_neighbors),
                                                                              len(_neighbors['negative'])))
@@ -324,11 +309,11 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
             #                                                          limits[1] - limits[0]) + pmin
             # if N is None:
             #     N = len(dataset_info['name'])
-            new_position = train(net, dataset_info['feature'][:N], dataset_info['name'].values.astype(str)[:N],
-                                 old_position[:N], new_position,
+            new_position = train(net, dataset_info['feature'][:max_display], dataset_info['name'].values.astype(str)[:max_display],
+                                 old_position[:max_display], new_position,
                                  idx_modified, idx_old_neighbors, idx_new_neighbors,
                                  _neighbors['negative'],
-                                 categories=dataset_info['categories'], label=dataset_info['label'][:N],
+                                 categories=dataset_info['categories'], label=dataset_info['label'][:max_display],
                                  lr=1e-3, experiment_id=experiment_id, socket_id=self.socket_id,
                                  scale_func=dataset_info['scale_func'])
 

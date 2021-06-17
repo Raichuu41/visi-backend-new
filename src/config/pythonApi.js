c715aa55b527fcbbbac9cee23bcfbdc6a8e10181
Mikail <ufaklik11@hotmail.de>
2021-06-17 23:01:46 +0200

    added backend production mode

--- src/config/pythonApi.js
+++ src/config/pythonApi.js
@@ -1,1 +1,8 @@
-export const pythonApi = 'http://localhost:8023';
+let baseUrl;
+if (process.env.NODE_ENV === 'production') {
+    baseUrl = '129.206.117.194';
+} else {
+    baseUrl = 'localhost'
+}
+
+export const pythonApi = `http://${baseUrl}:8023`;

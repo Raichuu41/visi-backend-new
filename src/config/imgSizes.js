1008ab94787b95284006c186c6ce96de5e4eac40
Mikail Cayoglu <info@mikail-cayoglu.de>
2021-08-16 22:15:34 +0200

    adjusted image sizes for binaries

--- src/config/imgSizes.js
+++ src/config/imgSizes.js
@@ -1,19 +1,14 @@
 export const imgSizes = [
-    10, // zoomStage: 0
-    20, // zoomStage: 1
-    30, // zoomStage: 2
-    40, // zoomStage: 3
-    50, // zoomStage: 4
-    60, // zoomStage: 5
-    70, // zoomStage: 6
-    80, // zoomStage: 7
-    90, // zoomStage: 8
-    100, // zoomStage: 9
-    // 110, // zoomStage: 10
-    // 120, // zoomStage: 11
-    // 130, // zoomStage: 12
-    // 140, // zoomStage: 13
-    // 150, // zoomStage: 14
+    5, // zoomStage: 0
+    10, // zoomStage: 1
+    15, // zoomStage: 2
+    20, // zoomStage: 3
+    25, // zoomStage: 4
+    30, // zoomStage: 5
+    35, // zoomStage: 6
+    40, // zoomStage: 7
+    45, // zoomStage: 8
+    50, // zoomStage: 9
 ];
 
 export default imgSizes;
